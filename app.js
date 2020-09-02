51755922dd2974c0b7ddc7aabc6a3c17c2173692
Calvin Sung <calvinsungvin@gmail.com>
2020-09-02 15:44:05 +0800

    refactor express router

--- app.js
+++ app.js
@@ -2,39 +2,29 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
-
+const routes = require('./routes')
+const session = require('express-session')
 const app = express()
 const PORT = 3000
 
+
+
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(routes)
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
-app.get('/users/login', (req, res) => {
-    res.render('login')
-  })
-  
-  app.post('/users/login', (req, res) => {
-    res.send('login')
-  })
-  
-  app.get('/users/register', (req, res) => {
-    res.render('register')
-  })
-  
-  app.post('/users/register', (req, res) => {
-    res.send('register')
-  })
-  
-  app.get('/users/logout', (req, res) => {
-    res.send('logout')
-  })  
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
